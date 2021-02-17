16a33666a225a95b97180f7dca49e00a2b3ceb11
RicXander75 <78831216+RicXander75@users.noreply.github.com>
2021-02-16 21:28:19 -0400

    Programa ahora se repite hasta salir

--- Program.cs
+++ Program.cs
@@ -14,110 +14,121 @@ namespace parcial01
             int banco;
             int limite;
             int monto;
-            //Inicializacion de variables 
-            cantidadMil = 0;
-            cantidadQuinientos = 0;
-            cantidadDoscientos = 0;
-            cantidadCien = 0;
-            do
+            do 
             {
-                Console.WriteLine("\n Bienvenidos a FDP INVERSMENTS \n Escoge tu banco \n 1: FDP INVERSMENTS \n 2: otro");
-                banco = int.Parse(Console.ReadLine());
-                if (banco == 1)  //Asignacion de limite para FDP
+                //Inicializacion de variables 
+                cantidadMil = 0;
+                cantidadQuinientos = 0;
+                cantidadDoscientos = 0;
+                cantidadCien = 0;
+                do
                 {
-                    limite = 20000;
-                    break;
-                }
-                else if (banco == 2) //Asignacion para otro 
-                {
-                    limite = 10000;
-                    break;
-                }
-            } while (true);
-            Console.WriteLine("¿Cuanto desea retirar?: ");
-            monto = int.Parse(Console.ReadLine());
-            if (monto <= limite) // Validar que el monto no pase del limite
-            {
-                if (monto % 100 == 0) //Validar que el monto es multiplo de 100
-                {
-                    while (monto > 0)//Bucle para que el programa vaya revisando si el monto puede ser restado del banco y si aun sobran de esos billetes
+                    Console.WriteLine("\n Bienvenidos a FDP INVERSMENTS \n Escoge tu banco \n 1: FDP INVERSMENTS \n 2: otro");
+                    banco = int.Parse(Console.ReadLine());
+                    if (banco == 1)  //Asignacion de limite para FDP
                     {
-                        if (monto - mil >= 0 && cantidadLimiteMil > 0)
+                        limite = 20000;
+                        break;
+                    }
+                    else if (banco == 2) //Asignacion para otro 
+                    {
+                        limite = 10000;
+                        break;
+                    }
+                } while (true);
+                Console.WriteLine("¿Cuanto desea retirar?: ");
+                monto = int.Parse(Console.ReadLine());
+                if (monto <= limite) // Validar que el monto no pase del limite
+                {
+                    if (monto % 100 == 0) //Validar que el monto es multiplo de 100
+                    {
+                        while (monto > 0)//Bucle para que el programa vaya revisando si el monto puede ser restado del banco y si aun sobran de esos billetes
                         {
-                            monto -= mil;
-                            cantidadLimiteMil--;
-                            cantidadMil++;
+                            if (monto - mil >= 0 && cantidadLimiteMil > 0)
+                            {
+                                monto -= mil;
+                                cantidadLimiteMil--;
+                                cantidadMil++;
+                            }
+                            else if (monto - quinientos >= 0 && cantidadLimiteQuinientos > 0)
+                            {
+                                monto -= quinientos;
+                                cantidadLimiteQuinientos--;
+                                cantidadQuinientos++;
+                            }
+                            else if (monto - doscientos >= 0 && cantidadLimiteDoscientos > 0)
+                            {
+                                monto -= doscientos;
+                                cantidadLimiteDoscientos--;
+                                cantidadDoscientos++;
+                            }
+                            else if (monto - cien >= 0 && cantidadLimiteCien > 0)
+                            {
+                                monto -= cien;
+                                cantidadLimiteCien--;
+                                cantidadCien++;
+                            }
+                            else
+                            {
+                                break;
+                            }
                         }
-                        else if (monto - quinientos >= 0 && cantidadLimiteQuinientos > 0)
+                        if (monto>0)//Si se acaban los billetes y el banco no puede devolver todo el monto, no se retira el dinero
                         {
-                            monto -= quinientos;
-                            cantidadLimiteQuinientos--;
-                            cantidadQuinientos++;
-                        }
-                        else if (monto - doscientos >= 0 && cantidadLimiteDoscientos > 0)
-                        {
-                            monto -= doscientos;
-                            cantidadLimiteDoscientos--;
-                            cantidadDoscientos++;
-                        }
-                        else if (monto - cien >= 0 && cantidadLimiteCien > 0)
-                        {
-                            monto -= cien;
-                            cantidadLimiteCien--;
-                            cantidadCien++;
+                            cantidadLimiteMil += cantidadMil;
+                            cantidadLimiteQuinientos += cantidadQuinientos;
+                            cantidadLimiteDoscientos += cantidadDoscientos;
+                            cantidadLimiteCien += cantidadCien;
+
+                            cantidadMil = 0;
+                            cantidadQuinientos = 0;
+                            cantidadDoscientos = 0;
+                            cantidadCien = 0;
+                            Console.WriteLine("No se puede retirar el monto especificado. ");
                         }
                         else
                         {
-                            break;
+                            Console.Write("Usted ha retirado: ");
                         }
-                    }
-                    if (monto>0)//Si se acaban los billetes y el banco no puede devolver todo el monto, no se retira el dinero
-                    {
-                        cantidadLimiteMil += cantidadMil;
-                        cantidadLimiteQuinientos += cantidadQuinientos;
-                        cantidadLimiteDoscientos += cantidadDoscientos;
-                        cantidadLimiteCien += cantidadCien;
+                        if (cantidadMil > 0)
+                        {
+                            Console.Write(cantidadMil + " de mil ");
+                        }
+                        if (cantidadQuinientos > 0)
+                        {
+                            Console.Write(cantidadQuinientos + " de quinientos ");
 
-                        cantidadMil = 0;
-                        cantidadQuinientos = 0;
-                        cantidadDoscientos = 0;
-                        cantidadCien = 0;
-                        Console.WriteLine("No se puede retirar el monto especificado. ");
+                        }
+                        if (cantidadDoscientos > 0)
+                        {
+                            Console.Write(cantidadDoscientos + " de doscientos ");
+
+                        }
+                        if (cantidadCien > 0)
+                        {
+                            Console.Write(cantidadCien + " de cien ");
+
+                        }
+
                     }
                     else
                     {
-                        Console.Write("Usted ha retirado: ");
+                        Console.WriteLine("El monto debe ser multiplo de 100");
                     }
-                    if (cantidadMil > 0)
-                    {
-                        Console.Write(cantidadMil + " de mil ");
-                    }
-                    if (cantidadQuinientos > 0)
-                    {
-                        Console.Write(cantidadQuinientos + " de quinientos ");
-
-                    }
-                    if (cantidadDoscientos > 0)
-                    {
-                        Console.Write(cantidadDoscientos + " de doscientos ");
-
-                    }
-                    if (cantidadCien > 0)
-                    {
-                        Console.Write(cantidadCien + " de cien ");
-
-                    }
-
                 }
                 else
                 {
-                    Console.WriteLine("El monto debe ser multiplo de 100");
+                    Console.WriteLine("No puede retirar mas de: " + limite);
                 }
-            }
-            else
-            {
-                Console.WriteLine("No puede retirar mas de: " + limite);
-            }                
+                Console.WriteLine("\n ¿Salir? [s/n]");
+                string salir = Console.ReadLine();
+                if (salir.ToLower().Equals("s"))
+                {
+                    break;
+                }
+            } while (true);
+            
+       
         }
     }
 }
